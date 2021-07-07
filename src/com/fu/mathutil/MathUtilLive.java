98a7e0bfab873c7c6a06f7804c1a74ded7ee8d16
TUANANH0 <anhtuan031999@gmail.com>
2021-07-07 17:36:00 +0700

    lan thu 2. test github

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -15,15 +15,19 @@ public class MathUtilLive {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         long result = MathUtil.getFactorial(5);
         System.out.println("expected 5! = 120; actual: " + result);
         System.out.println("expected 6! = 720; actual: " + MathUtil.getFactorial(6));
         System.out.println("expected 0! = 1; actual: " + MathUtil.getFactorial(0));
         //MathUtil.getFactorial(-5);//ném ngoại lệ 
+        //thêm code sau lần đầu tiên 5h35p
+        System.out.println("expected 1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected 3! = 6; actual: " + MathUtil.getFactorial(3));
+
         //kỹ thuật kiểm thử phần mềm: ước lượng xem giá trị trả về của hàm là gì 
         //expected value: 5! = 120
         // if expected == actual, hàm ngon trong tình hướng này
     }
-    
+
 }
